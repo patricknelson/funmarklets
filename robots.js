faaca797dba02991811265f6a7fe448d60494ec9
Patrick Nelson <pat@catchyour.com>
2014-10-27 02:30:11 -0400

    Updated robots bookmarklet to be consistent each time it's clicked.

--- robots.js
+++ robots.js
@@ -2,7 +2,7 @@
 	var killHumans = function () {
 		for (var b = document.images, a = 0; a < b.length; a++) {
 			var c = b[a].width, d = b[a].height;
-			if (c && d)b[a].src = 'https://robohash.org/' + Math.random() + '?size=' + c + 'x' + d
+			if (c && d)b[a].src = 'https://robohash.org/' + encodeURIComponent(b[a].src) + '?size=' + c + 'x' + d
 		}
 		return false
 	};
